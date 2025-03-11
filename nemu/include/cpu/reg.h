3ff3c95bc5a70569c23ef87048f7d8deb603dc6b
tracer-ics2017 <tracer@njuics.org>
2025-03-11 15:10:27 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  15:10:27 up 13 min,  1 user,  load average: 1.64, 1.35, 0.84 91393536758d7320dec10d3e5b0fce8d85468bd4

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,22 +15,46 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+  union {
+    union {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    struct{
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };
+  };
 
   vaddr_t eip;
+	unsigned int cs;
+
+	union
+  {
+			rtlreg_t eflags_init;
+      struct
+      {
+          unsigned int CF:1;
+          unsigned int ZF:1;
+          unsigned int SF:1;
+          unsigned int IF:1;
+          unsigned int OF:1;
+      };
+  }eflags;
+
+	struct{
+		uint16_t limit;
+		uint32_t base;
+	}idtr;
 
 } CPU_state;
+
 
 extern CPU_state cpu;
 
