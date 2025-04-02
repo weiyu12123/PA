5b87fafa1b506b11520a3d88dc4b69cd468903ee
tracer-ics2017 <tracer@njuics.org>
2025-04-02 20:35:30 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:35:30 up  5:40,  1 user,  load average: 0.40, 0.53, 0.61 263c3eea9a16302a13b18f0d7f5cefcc4c34939

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,9 +1,11 @@
 #include "cpu/exec.h"
 
 make_EHelper(test) {
-  TODO();
-
-  print_asm_template2(test);
+    rtl_and(&t2, &id_dest->val, &id_src->val);
+    rtl_update_ZFSF(&t2, id_dest->width);
+    rtl_set_CF(&tzero);
+    rtl_set_OF(&tzero);
+    print_asm_template2(test);
 }
 
 make_EHelper(and) {
