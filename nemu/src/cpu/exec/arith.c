40e5bb641214cffb9be7faed889be1843d4753cf
tracer-ics2017 <tracer@njuics.org>
2025-04-03 21:35:14 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:35:14 up  6:44,  1 user,  load average: 1.35, 1.09, 0.89 5f40c33c74c230594fa145b81f87762aea9532

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,39 +1,6 @@
 #include "cpu/exec.h"
 
-make_EHelper(add) {
-
-  rtl_add(&t2, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &t2);
-  rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_sltu(&t0, &t2, &id_dest->val);
-  rtl_set_CF(&t0);
-  rtl_xor(&t0, &id_src->val, &t2);
-  rtl_xor(&t1, &id_dest->val, &t2);
-  rtl_and(&t0, &t0, &t1);
-  rtl_msb(&t0, &t0, id_dest->width);
-  rtl_set_OF(&t0);
-  print_asm_template2(add);
-}
-
-make_EHelper(sub) {
-    rtl_sub(&t2, &id_dest->val, &id_src->val);
-    operand_write(id_dest, &t2);
-
-    rtl_update_ZFSF(&t2, id_dest->width);
-
-    rtl_sltu(&t0, &t2, &id_dest->val);
-    rtl_set_CF(&t0);
-
-    rtl_xor(&t0, &id_dest->val, &id_src->val);
-    rtl_xor(&t1, &id_dest->val, &t2);
-    rtl_and(&t0, &t0, &t1);
-    rtl_msb(&t0, &t0, id_dest->width);
-    rtl_set_OF(&t0);
-
-    print_asm_template2(sub);
-}
-
-make_EHelper(cmp) {
+static inline void eflags_modify(){
   rtl_sub(&t2, &id_dest->val, &id_src->val);
 
   // ZFSF
@@ -49,6 +16,34 @@ make_EHelper(cmp) {
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
+}
+
+make_EHelper(add) {
+  rtl_add(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_sltu(&t0, &t2, &id_dest->val);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_src->val, &t2);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
+
+  print_asm_template2(add);
+}
+
+make_EHelper(sub) {
+  eflags_modify();
+  operand_write(id_dest, &t2);
+
+  print_asm_template2(sub);
+}
+
+make_EHelper(cmp) {
+  eflags_modify();
 
   print_asm_template2(cmp);
 }
