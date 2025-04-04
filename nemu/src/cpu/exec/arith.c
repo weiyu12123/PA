d69dd68e0bdaf78565ffd2630e91426d229c2dcf
tracer-ics2017 <tracer@njuics.org>
2025-04-04 18:53:30 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:53:30 up 12:09,  1 user,  load average: 1.12, 0.60, 0.41 3aaea045186de83be499ab4d101b0f5b187652cd

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -36,7 +36,21 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  eflags_modify();
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
+
+  // ZFSF
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  // CF
+  rtl_sltu(&t0, &id_dest->val, &id_src->val);
+  rtl_set_CF(&t0);
+
+  // OF
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
   operand_write(id_dest, &t2);
 
   print_asm_template2(sub);
