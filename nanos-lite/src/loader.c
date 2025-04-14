466dfabbb74615393c9b3f700cdeb19e64c71c2b
tracer-ics2017 <tracer@njuics.org>
2025-04-14 16:50:17 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:50:17 up 1 day,  3:10,  1 user,  load average: 1.27, 0.54, 0.43 47757390d950e9b2fd49425245cdd11477e3855d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,15 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  
+  ramdisk_read(DEFAULT_ENTRY,0,RAMDISK_SIZE);
+  
   return (uintptr_t)DEFAULT_ENTRY;
 }
