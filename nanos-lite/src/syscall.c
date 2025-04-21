7bb0e22a73bf5aa775474e4d7346b8d445ee876f
tracer-ics2017 <tracer@njuics.org>
2025-04-21 21:29:54 +0800

    >  run 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:29:54 up 1 day,  8:55,  1 user,  load average: 0.54, 0.85, 0.68 de26a9ff88f373809945f780862d3caa228c4803

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,17 +9,17 @@ void sys_exit(int a){
   _halt(a);
 }
 
-uintptr_t sys_write(int fd, const void *buf, size_t count){
-  uintptr_t i = 0;
+int sys_write(int fd, void *buf, size_t len) {
+	if(fd == 1 || fd == 2){
+		char c;
+		for(int i = 0; i < len; i++) {
+			memcpy(&c ,buf + i, 1);
+			_putc(c);
+		}
+		return len;
+	}
 
-  if (fd == 1 || fd == 2) { // stdout or stderr
-    for (; i < count; i++) {
-      _putc(((char *)buf)[i]); // _putc 是 AM 提供的字符输出函数
-    }
-    return count; // 成功写入的字节数
-  }
-
-  return -1; // 对于不支持的 fd，返回错误
+	return -1;			
 }
 
 
