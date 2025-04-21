b71eaf8cb7085dbadd2a16d145420578b8435757
tracer-ics2017 <tracer@njuics.org>
2025-04-21 22:02:13 +0800

    >  run 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  22:02:12 up 1 day,  9:28,  1 user,  load average: 0.53, 0.56, 0.62 c93b469037e435436a5a363935c2402e9a3764a5

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,6 +12,8 @@ void sys_exit(int a){
 int sys_write(int fd, void *buf, size_t len) {
 	if(fd == 1 || fd == 2){
     Log("buffer:%s", (char*)buf);
+    Log("sys_write: fd=%d, len=%d, buffer:%s", fd, len, (char*)buf);
+
 		for(int i = 0; i < len; i++) {
 			_putc(((char*) buf)[i]);
 		}
