84d3f03920711f4eb91bd48917df92a1ad6bcbe5
tracer-ics2017 <tracer@njuics.org>
2025-05-11 19:07:28 +0800

    >  run 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:07:28 up 2 days,  7:05,  1 user,  load average: 2.16, 1.78, 1.68 1b4da9b26fd1ba38af14ceafc7f57cf1741aad78

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -27,7 +27,8 @@ int sys_write(int fd, void *buf, size_t len) {
 
 
 int sys_brk(int addr) {
-  return 0;
+  extern int mm_brk(uint32_t new_brk);
+  return mm_brk(addr);
 }
 
 int sys_open(const char *pathname){
