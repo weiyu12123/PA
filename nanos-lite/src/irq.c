4d250b13a106aa39788e371aab4a2ad5298ead40
tracer-ics2017 <tracer@njuics.org>
2025-05-11 20:44:22 +0800

    >  run 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:44:21 up 2 days,  8:42,  1 user,  load average: 1.92, 1.78, 1.95 6bb564a3749d4a699edaa9e82c2c7232c41083a

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,10 +5,10 @@ extern _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
     switch (e.event) {
         case _EVENT_SYSCALL:
-            return do_syscall(r);
+            do_syscall(r);
+            return schedule(r);
         case _EVENT_TRAP:
             printf("event:self - trapped\n");
-            do_syscall(r);
             return schedule(r);
         default:
             panic("Unhandled event ID = %d", e.event);
