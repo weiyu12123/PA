c6708be3a640a45fe8583a3390bef50f4d575900
tracer-ics2017 <tracer@njuics.org>
2025-05-27 20:20:00 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:20:00 up 8 min,  1 user,  load average: 0.55, 0.58, 0.38 c1fb60b0a1b6d24b2a15918cfc49ee78a0c035b6

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -15,40 +15,47 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-//	Log("%d",id_dest->reg); 
-  switch(id_dest->reg){
-  	case 0:
-		cpu.CR0=id_src->val;
-		break;
-	case 3:
-		cpu.CR3=id_src->val;
-		break;	
-  	default:Log("gg");assert(0);
+  switch (id_dest->reg) {
+    case 0:  // CR0
+      cpu.CR0 = id_src->val;
+      break;
+    case 3:  // CR3
+      cpu.CR3 = id_src->val;
+      break;
+    default:
+      Log("Unsupported control register: CR%d", id_dest->reg);
+      assert(0);
   }
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
+
 make_EHelper(mov_cr2r) {
- 
-switch(id_dest->reg){
-		case 0:
-		t0=cpu.CR0;
-		rtl_sr(id_src->reg,4,&t0);
-		break;
-		case 2:
-		t0=cpu.CR3;
-		rtl_sr(id_src->reg,4,&t0);
-		break;
-		default:Log("gg");assert(0);
-}
+  rtlreg_t t0;
+
+  switch (id_src->reg) {
+    case 0:  // CR0
+      t0 = cpu.CR0;
+      break;
+    case 3:  // CR3
+      t0 = cpu.CR3;
+      break;
+    default:
+      Log("Unsupported control register: CR%d", id_src->reg);
+      assert(0);
+  }
+
+  rtl_sr(id_dest->reg, 4, &t0);  // 写入 GPR
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
-  diff_test_skip_qemu();
+  diff_test_skip_qemu();  // 跳过 QEMU 对比
 #endif
 }
+
+
 extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 
 make_EHelper(int) {
