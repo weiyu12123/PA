357ac4af1ae232bba844a37596b5058d6abbdda2
tracer-ics2017 <tracer@njuics.org>
2025-05-27 20:31:04 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:31:04 up 19 min,  1 user,  load average: 0.05, 0.36, 0.42 8f45627e954a7721bbf595b4111e8689e669c112

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -150,7 +150,7 @@ opcode_entry opcode_table [512] = {
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x18 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x20 */	IDEXW(G2E,mov_cr2r,4), EMPTY,IDEXW(E2G,mov_r2cr,4), EMPTY,
+  /* 0x20 */  IDEX(mov_load_cr,mov), EMPTY, IDEX(mov_store_cr,mov_store_cr), EMPTY,
   /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x28 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
