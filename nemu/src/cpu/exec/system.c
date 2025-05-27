e76e6dc5e5f10ba7f934d65b3f49ab4df55c08ba
tracer-ics2017 <tracer@njuics.org>
2025-05-27 20:28:05 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:28:05 up 16 min,  1 user,  load average: 0.36, 0.54, 0.48 f757821cb7e6ad6fe4079119d8a6fea12567947

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -15,7 +15,7 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  /*switch (id_dest->reg) {
+  switch (id_dest->reg) {
     case 0:  // CR0
       cpu.CR0 = id_src->val;
       break;
@@ -27,12 +27,12 @@ make_EHelper(mov_r2cr) {
       assert(0);
   }
 
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);*/
+  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 
 make_EHelper(mov_cr2r) {
-  /*rtlreg_t t0;
+  rtlreg_t t0;
 
   switch (id_src->reg) {
     case 0:  // CR0
@@ -52,7 +52,7 @@ make_EHelper(mov_cr2r) {
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();  // 跳过 QEMU 对比
-#endif*/
+#endif
 }
 
 
