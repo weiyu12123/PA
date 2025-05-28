343e842164adc4553ffc1ed61aaba41e05279544
tracer-ics2017 <tracer@njuics.org>
2025-05-28 18:45:44 +0800

    >  compile 2211752 weiyu Linux weiyu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:45:44 up 9 min,  1 user,  load average: 0.72, 0.70, 0.40 88309e98ddc3d70f52802a2aa2d94c76e70345f

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-    assert(-((int64_t)1 << 32) < ((int64_t) a * (int64_t) b) >> 16 &&
-                   ((int64_t) a * (int64_t) b) >> 16 < ((int64_t)1 << 32));
+    /*assert(-((int64_t)1 << 32) < ((int64_t) a * (int64_t) b) >> 16 &&
+                   ((int64_t) a * (int64_t) b) >> 16 < ((int64_t)1 << 32));*/
     return ((int64_t) a * (int64_t) b) >> 16;
 }
 
